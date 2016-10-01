b412622e1646c3bfe55ab0163f725061c43fae59
tsengeagle <tsengeagle@gmail.com>
2016-10-01 21:24:04 +0800

    新增元件：分攤總表

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ApportionMasterComponent } from './apportion-master/apportion-master.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApportionMasterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/apportion-master/apportion-master.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { ApportionMasterComponent } from './apportion-master.component';
+
+describe('Component: ApportionMaster', () => {
+  it('should create an instance', () => {
+    let component = new ApportionMasterComponent();
+    expect(component).toBeTruthy();
+  });
+});
