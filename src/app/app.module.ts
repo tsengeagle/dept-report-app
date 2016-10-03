23284dfb6bca35aff5adeab907f3e89698f3975c
tsengeagle <tsengeagle@gmail.com>
2016-10-03 14:58:48 +0800

    拿到component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ApportionMasterComponent } from './apportion-master/apportion-master.component';
-import { ApportionDetailComponent } from './apportion-detail/apportion-detail.component';
-import { ApportionService } from './apportion.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ApportionMasterComponent,
-    ApportionDetailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [ ApportionService ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
